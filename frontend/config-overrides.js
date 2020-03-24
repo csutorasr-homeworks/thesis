a43c6c19f3e153598b35de8e6d1bc6b60e3af65f
Csutorás Robin <csutorasr@gmail.com>
2020-03-24 21:12:58 +0100

    feat: eslint with prettier is added

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -5,7 +5,7 @@ module.exports = function override(config, env) {
 
   config.resolve.alias = {
     ...config.resolve.alias,
-    'react-dom': '@hot-loader/react-dom'
+    'react-dom': '@hot-loader/react-dom',
   };
 
   return config;
