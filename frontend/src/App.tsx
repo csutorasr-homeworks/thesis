2c34af4c9df8075334aa06792eb70facf194d6d1
Csutorás Robin <csutorasr@gmail.com>
2020-03-24 18:59:45 +0100

    react starter with router

---
+++ frontend/src/App.tsx
@@ -0,0 +1,41 @@
+import './App.css';
+
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">home</Link>
+          <Link to="/test">test</Link>
+          <Switch>
+            <Route exact path="/">
+              <p>homepage</p>
+            </Route>
+            <Route path="/test">
+              <p>test</p>
+            </Route>
+          </Switch>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
