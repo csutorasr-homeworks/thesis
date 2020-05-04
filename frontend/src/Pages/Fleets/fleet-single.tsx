32030a7dec04de27964f436775e5f86832ef34d4
Csutorás Robin <csutorasr@gmail.com>
2020-05-04 21:19:20 +0200

    feat: single page

--- frontend/src/Pages/Fleets/fleet-single.tsx
+++ frontend/src/Pages/Fleets/fleet-single.tsx
@@ -1,7 +1,19 @@
 import { Link, useParams } from 'react-router-dom';
+import ErrorComponent from '../../Components/Error';
 import React from 'react';
+import useAxios from 'axios-hooks';
 
 export default function FleetSingle() {
   const { fleetId } = useParams();
-  return <Link to={`/fleets/${fleetId}/edit`}>Edit</Link>;
+  const [{ data: fleet, loading, error }, refetch] = useAxios<{
+    id: string;
+    name: string;
+  }>(`/fleets/${fleetId}`);
+  return (
+    <>
+      <ErrorComponent loading={loading} error={error} refetch={refetch} />
+      <h1>{fleet?.name}</h1>
+      <Link to={`/fleets/${fleetId}/edit`}>Edit</Link>
+    </>
+  );
 }
