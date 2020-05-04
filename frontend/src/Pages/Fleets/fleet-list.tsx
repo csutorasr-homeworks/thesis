5ecf35a20918766a99ddecd9d76368e51934c919
Csutorás Robin <csutorasr@gmail.com>
2020-05-04 20:57:05 +0200

    feat: fleet list

---
+++ frontend/src/Pages/Fleets/fleet-list.tsx
@@ -0,0 +1,27 @@
+import { Link, Redirect } from 'react-router-dom';
+import ErrorComponent from '../../Components/Error';
+import React from 'react';
+import useAxios from 'axios-hooks';
+
+export default function FleetList() {
+  const [{ data: fleets, loading, error }, refetch] = useAxios<
+    { id: string; name: string }[]
+  >('/fleets');
+  if (fleets && fleets.length === 1) {
+    return <Redirect to={`/fleets/${fleets[0].id}`} />;
+  }
+  return (
+    <>
+      <ErrorComponent loading={loading} error={error} refetch={refetch} />
+      <ul>
+        {!loading &&
+          !error &&
+          fleets.map((x) => (
+            <li key={x.id}>
+              <Link to={`/fleets/${x.id}`}>{x.name}</Link>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+}
--- frontend/src/Pages/Home/index.tsx
+++ frontend/src/Pages/Home/index.tsx
@@ -1,22 +1,11 @@
-import ErrorComponent from '../../Components/Error';
+import FleetList from '../Fleets/fleet-list';
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-import useAxios from 'axios-hooks';
 
 export default function Home() {
-  const [{ data: fleets, loading, error }, refetch] = useAxios<
-    { id: string; name: string }[]
-  >('/fleets');
-  if (fleets && fleets.length === 1) {
-    return <Redirect to={`/fleets/${fleets[0].id}`} />;
-  }
   return (
     <div>
       Home
-      <ErrorComponent loading={loading} error={error} refetch={refetch} />
-      <ul>
-        {!loading && !error && fleets.map((x) => <li key={x.id}>{x.name}</li>)}
-      </ul>
+      <FleetList />
     </div>
   );
 }
