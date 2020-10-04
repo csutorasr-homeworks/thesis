c843415b0d1760f552f0e91dd5987bac38ddbe0f
Robin Csutorás <robin.csutoras@supercharge.io>
2020-10-04 21:50:30 +0200

    fix: remove warning for nested a tag

--- frontend/src/Pages/Fleets/Cars/cars-list.tsx
+++ frontend/src/Pages/Fleets/Cars/cars-list.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import useAxios from 'axios-hooks';
 import React from 'react';
 import { Button, ButtonGroup, Card, Col, Row } from 'react-bootstrap';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 
 import ErrorComponent from '../../../Components/Error';
 
@@ -15,6 +15,7 @@ export interface CarRowVm {
 }
 
 export default function CarsList(): JSX.Element {
+  const history = useHistory();
   const { fleetId } = useParams<{ fleetId: string }>();
   const [{ data: fleets, loading, error }, refetch] = useAxios<CarRowVm[]>(
     `/fleets/${fleetId}/cars`
@@ -26,40 +27,44 @@ export default function CarsList(): JSX.Element {
           <Row>
             {fleets.map((x) => (
               <Col key={x.id} sm={6} md={4} lg={3} className="mb-4 no-hover">
-                <Link to={`/fleets/${fleetId}/cars/${x.id}`}>
-                  <Card>
-                    <Card.Body>
-                      <Card.Title>{x.licensePlateNumber}</Card.Title>
-                      <ButtonGroup className="card-button">
-                        <Button>
-                          <FontAwesomeIcon icon={faEye} />
-                        </Button>
-                        <Button
-                          variant="secondary"
-                          as={Link}
-                          to={`/fleets/${fleetId}/cars/${x.id}/edit`}
-                        >
-                          <FontAwesomeIcon icon={faEdit} />
-                        </Button>
-                      </ButtonGroup>
-                    </Card.Body>
-                  </Card>
-                </Link>
+                <Card
+                  onClick={() =>
+                    history.push(`/fleets/${fleetId}/cars/${x.id}`)
+                  }
+                  style={{ cursor: 'pointer' }}
+                >
+                  <Card.Body>
+                    <Card.Title>{x.licensePlateNumber}</Card.Title>
+                    <ButtonGroup className="card-button">
+                      <Button>
+                        <FontAwesomeIcon icon={faEye} />
+                      </Button>
+                      <Button
+                        variant="secondary"
+                        as={Link}
+                        to={`/fleets/${fleetId}/cars/${x.id}/edit`}
+                      >
+                        <FontAwesomeIcon icon={faEdit} />
+                      </Button>
+                    </ButtonGroup>
+                  </Card.Body>
+                </Card>
               </Col>
             ))}
           </Row>
           <Row>
             <Col sm={6} md={4} lg={3} className="mb-4 no-hover">
-              <Link to={`/fleets/${fleetId}/cars/new`}>
-                <Card>
-                  <Card.Body>
-                    <Card.Title>Add new car</Card.Title>
-                    <Button className="card-button">
-                      <FontAwesomeIcon icon={faPlusSquare} />
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Link>
+              <Card
+                onClick={() => history.push(`/fleets/${fleetId}/cars/new`)}
+                style={{ cursor: 'pointer' }}
+              >
+                <Card.Body>
+                  <Card.Title>Add new car</Card.Title>
+                  <Button className="card-button">
+                    <FontAwesomeIcon icon={faPlusSquare} />
+                  </Button>
+                </Card.Body>
+              </Card>
             </Col>
           </Row>
         </>
