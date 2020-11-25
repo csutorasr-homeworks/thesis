4856285629b36fa31666736b45e56537efb26ff2
Robin Csutorás <robin.csutoras@supercharge.io>
2020-11-26 00:20:47 +0100

    test: add backend unit tests

---
+++ backend/Flottapp/Flottapp.UnitTests/Fleet/CreateFleet.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Flottapp.Application.Fleet;
+using Flottapp.Infrastucture.Commands;
+using Moq;
+using Xunit;
+
+namespace Flottapp.UnitTests
+{
+    public class CreateFleet
+    {
+        private readonly Mock<IFleetStore> fleetStoreMock;
+        private readonly CreateFleetCommand.Handler handler;
+
+        public CreateFleet()
+        {
+            fleetStoreMock = new Mock<IFleetStore>();
+            handler = new CreateFleetCommand.Handler(fleetStoreMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateFleetSuccess()
+        {
+            var cancellationToken = new CancellationToken();
+            fleetStoreMock.Setup(x => x.CreateFleet("New fleet name", It.Is<Model.AuthorizationData>(x => x.Authority == "test-authority" && x.Id == "123"), cancellationToken)).ReturnsAsync("new Id");
+            var result = await handler.Handle(new CreateFleetCommand
+            {
+                AuthorizationData = new Model.AuthorizationData
+                {
+                    Authority = "test-authority",
+                    Id = "123",
+                },
+                Name = "New fleet name",
+            }, cancellationToken);
+            Assert.Equal("new Id", result);
+        }
+    }
+}
---
+++ backend/Flottapp/Flottapp.UnitTests/Fleet/DeleteFleet.cs
@@ -0,0 +1,56 @@
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Flottapp.Application.Fleet;
+using Flottapp.Application.Fleet.Exceptions;
+using Flottapp.Infrastucture.Commands;
+using Moq;
+using Xunit;
+
+namespace Flottapp.UnitTests
+{
+    public class DeleteFleet
+    {
+        private readonly Mock<IFleetStore> fleetStoreMock;
+        private readonly DeleteFleetCommand.Handler handler;
+
+        public DeleteFleet()
+        {
+            fleetStoreMock = new Mock<IFleetStore>();
+            handler = new DeleteFleetCommand.Handler(fleetStoreMock.Object);
+        }
+
+        [Fact]
+        public async Task DeleteFleetSuccess()
+        {
+            var cancellationToken = new CancellationToken();
+            fleetStoreMock.Setup(x => x.DeleteFleet("1234", It.Is<Model.AuthorizationData>(x => x.Authority == "test-authority" && x.Id == "123"), cancellationToken));
+            await handler.Handle(new DeleteFleetCommand
+            {
+                AuthorizationData = new Model.AuthorizationData
+                {
+                    Authority = "test-authority",
+                    Id = "123",
+                },
+                Id = "1234",
+            }, cancellationToken);
+        }
+
+        [Fact]
+        public async Task DeleteFleetFailure()
+        {
+            var cancellationToken = new CancellationToken();
+            fleetStoreMock.Setup(x => x.DeleteFleet("1234", It.Is<Model.AuthorizationData>(x => x.Authority == "test-authority" && x.Id == "123"), cancellationToken)).ThrowsAsync(new FleetNotFoundException());
+            await Assert.ThrowsAsync<FleetNotFoundException>(async () => await handler.Handle(new DeleteFleetCommand
+            {
+                AuthorizationData = new Model.AuthorizationData
+                {
+                    Authority = "test-authority",
+                    Id = "123",
+                },
+                Id = "1234",
+            }, cancellationToken));
+        }
+    }
+}
